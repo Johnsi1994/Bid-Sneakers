1ec1c8223e66d02733e4032d2c03161ddb63d98d
Johnsi Tu <jshop1994212@gmail.com>
2019-04-02 20:30:27 +0800

    set BottomNavigation onSelectedListener without function

--- BID/app/src/main/java/com/johnson/bid/MainActivity.java
+++ BID/app/src/main/java/com/johnson/bid/MainActivity.java
@@ -1,12 +1,14 @@
 package com.johnson.bid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
-public class MainActivity extends AppCompatActivity implements MainContract.View{
+    private BottomNavigationView mBottomNavigation;
 
     private MainContract.Presenter mPresenter;
 
@@ -14,8 +16,9 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        init();
+    }
 
     @Override
     public void openCenterUi() {
@@ -26,4 +29,32 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     public void setPresenter(MainContract.Presenter presenter) {
         mPresenter = checkNotNull(presenter);
     }
+
+    private void init() {
+        setBottomNavigation();
+    }
+
+    private void setBottomNavigation() {
+
+        mBottomNavigation = findViewById(R.id.bottom_navigation_main);
+        mBottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mBottomNavigation.setItemIconTintList(null); //去除BottomNavigation上面icon的顏色
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = menuItem -> {
+
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_center:
+                break;
+            case R.id.navigation_trade:
+                break;
+            case R.id.navigation_message:
+                break;
+            case R.id.navigation_settings:
+                break;
+            default:
+        }
+        return false;
+    };
 }
